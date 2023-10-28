47792127dbd67f459117fe3e8ca5f3a539a9a96e
ANDRES BECERRA <andresbecerra7200@gmail.com>
2023-10-27 22:26:46 -0400

    Delete bootstrap - reset CSS

--- src/app/components/banners/banner.component.ts
+++ src/app/components/banners/banner.component.ts
@@ -6,4 +6,23 @@ import { Component } from "@angular/core";
   styleUrls: ["./banner.component.scss"]
 })
 
-export class BannerComponent { }
+export class BannerComponent {
+  categories = [
+    {
+      img: '/assets/images/products/mushrooms.png',
+      name: 'Dried Magic Mushrooms'
+    },
+    {
+      img: '/assets/images/products/shrom-edibles.png',
+      name: 'Shrom Edibles'
+    },
+    {
+      img: '/assets/images/products/magic-mushrooms.png',
+      name: 'Magic Musrooms Tea'
+    },
+    {
+      img: '/assets/images/products/microdosing-capsules.png',
+      name: 'Microdosing Capsules'
+    },
+  ]
+}
